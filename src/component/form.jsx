8dd742eaf15ccd9c602ecfd42c96ff469dad24c3
Naveed69 <naveed.desai.69@gmail.com>
2024-11-04 19:33:39 +0530

    Edited Component

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,8 +1,18 @@
 export const Form=()=>{
+    const handleForm=()=>{
+
+    }
     return(
         <div>
             <h1><u><i>Fill Form</i></u></h1>
-
+            <label>Name:<input type="text" name="username" placeholder="Enter Name"></input><br/>
+            <label>Age:<input type="text" name="age" placeholder="Enter Age"></input></label></label><br/>
+            <label>Select Doctor:</label>
+            <select >
+                <option value='naveed'>Dr.Naveed</option>
+                <option value='roma'>Dr.Roma</option>
+            </select>
+            <br/><button type="button" onClick={handleForm}>Submit</button>
         </div>
     )
 }
