9fa93028487c59429653aa035c2fe2aecc16410f
Naveed69 <naveed.desai.69@gmail.com>
2024-11-14 00:47:20 +0530

    Added form

--- src/component/form.jsx
+++ src/component/form.jsx
@@ -1,10 +1,11 @@
 export const Form=()=>{
-    const handleForm=()=>{
-
+    const handleForm=(event)=>{
+        console.log(event);
     }
     return(
         <div>
             <h1><u><i>Fill Form</i></u></h1>
+            <form onSubmit={(e)=>handleForm}>
             <label>Name:<input type="text" name="username" placeholder="Enter Name"></input><br/>
             <label>Age:<input type="text" name="age" placeholder="Enter Age"></input></label></label><br/>
             <label>Select Doctor:</label>
@@ -13,8 +14,8 @@ export const Form=()=>{
                 <option value='roma'>Dr.Roma</option>
             </select><br/>
             <label>Date:<input type="date"></input></label>
-            <br/><button type="button" onClick={handleForm}>Submit</button>
-
+            <br/><button type="submit" >Submit</button>
+            </form>
         </div>
     )
 }
